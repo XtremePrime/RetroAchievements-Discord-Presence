dc60760de22dcd17ccfba69e2263ec1e3dcbd44b
Avery <34522242+jacket430@users.noreply.github.com>
2024-06-10 20:39:43 -0600

    Restored RANes, added RARPC.

--- RANes.py
+++ RANes.py
@@ -1,82 +1,58 @@
-import argparse
-from colorama import Fore, Style, init
-from datetime import datetime
-from pprint import pprint
-from pypresence import Presence 
-import re
+# XtremePrime 2024
+# -- You will need to setup a discord application for this in order to work. To do so, follow the instructions in the README.md
+# -- Please note: This will not be 100% accurate as it only uses latest data from your retro achievements page
+
+import io
 import requests
+import sys
 import time
+from pypresence import Presence 
 
-init(autoreset=True)
+# The first argument of your python call should be your Username, then your API_KEY, then your Discord Application Client ID
+# Ex: 
+# > python RANes.py AverageUser 0X0X0X0X0X0X0X0X 123456789
+USERNAME=str(sys.argv[1])
+API_KEY=str(sys.argv[2])
+RPC_CLIENT_ID = str(sys.argv[3])
 
-def get_data(url):
-    response = requests.get(url)
+profile_url = "https://retroachievements.org/API/API_GetUserProfile.php?u={0}&y={1}&z={2}".format(USERNAME, API_KEY, USERNAME)
+print(profile_url)
+
+RPC = Presence(RPC_CLIENT_ID)
+print(">Connecting to Discord App...")
+RPC.connect()
+print(">Connected")
+
+status = True
+
+while(status):
+    print(">GET profile game activity...")
+    response = requests.get(profile_url)
     if response.status_code == 200:
-        return response.json()
-    else:
-        print(Fore.RED + f"Failed to fetch data from {url}, status code: {response.status_code}")
-        return None
+        data = response.json()
+        print(">Result: {0}".format(data["RichPresenceMsg"]))
 
-def sanitize_console_name(console_name):
-    sanitized_name = re.sub('[^0-9a-zA-Z]+', '', console_name)
-    return sanitized_name.lower()
+        game_params = "?z={0}&y={1}&i={2}".format(USERNAME, API_KEY, data["LastGameID"])
+        game_url = "{0}?{1}".format("https://retroachievements.org/API/API_GetGame.php", game_params)
+        print(">GET game data...")
+        game_response = requests.get(game_url)
 
-def update_presence(RPC, data, game_data, start_time, args):
-    release_date = datetime.strptime(game_data['Released'], '%B %d, %Y')
-    year_of_release = release_date.year
-    details = f"{game_data['GameTitle']} ({year_of_release})"
+        if game_response.status_code == 200:
+            game_data = game_response.json()
+            print(">Result: {0}".format(game_data["GameTitle"]))
+
+            # Print each key-value pair in the game_data dictionary
+            for key, value in game_data.items():
+                print(f"{key}: {value}")
+        else:
+            print("Failed to fetch profile data:", response.status_code)
+            status = False
+            break;
+
     RPC.update(
         state=data["RichPresenceMsg"],
-        details=details,
-        start=start_time,
-        large_image="ra_logo",
-        large_text=f"Released {game_data['Released']}, Developed by {game_data['Developer']}, Published by {game_data['Publisher']}",
-        small_image=sanitize_console_name(game_data['ConsoleName']),
-        small_text=game_data['ConsoleName'],
-        buttons=[{"label": "View RA Profile", "url": f"https://retroachievements.org/user/{args.username}"}]
-    )
+        details=game_data["GameTitle"],
+        )
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('username')
-    parser.add_argument('api_key')
-    parser.add_argument('--debug', action='store_true')
-    args = parser.parse_args()
-
-    profile_url = f"https://retroachievements.org/API/API_GetUserProfile.php?u={args.username}&y={args.api_key}&z={args.username}"
-
-    start_time = int(time.time())
-
-    RPC = Presence("1249693940299333642")
-    print(Fore.CYAN + "Connecting to Discord App...")
-    RPC.connect()
-    print(Fore.MAGENTA + "Connected!")
-
-    while True:
-        print(Fore.CYAN + f"Fetching {args.username}'s RetroAchievements activity...")
-        data = get_data(profile_url)
-        if data is None:
-            break
-
-        print(Fore.MAGENTA + f"Result: {data['RichPresenceMsg']}")
-
-        game_params = f"?z={args.username}&y={args.api_key}&i={data['LastGameID']}"
-        game_url = f"https://retroachievements.org/API/API_GetGame.php{game_params}"
-        print(Fore.CYAN + "Fetching game data...")
-        game_data = get_data(game_url)
-        if game_data is None:
-            break
-
-        print(Fore.MAGENTA + f"Result: {game_data['GameTitle']}")
-
-        if args.debug:
-            print(Fore.YELLOW + "Debug game data:")
-            pprint(game_data)
-
-        update_presence(RPC, data, game_data, start_time, args)
-
-        print(Fore.CYAN + "Sleeping...")
-        time.sleep(30)
-
-if __name__ == "__main__":
-    main()
+    print(">Sleeping...")
+    time.sleep(30)
---
+++ RARPC.py
@@ -0,0 +1,89 @@
+import argparse
+from colorama import Fore, Style, init
+import configparser
+from datetime import datetime
+from pprint import pprint
+from pypresence import Presence 
+import re
+import requests
+import time
+
+init(autoreset=True)
+
+def get_data(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(Fore.RED + f"Failed to fetch data from {url}, status code: {response.status_code}")
+        return None
+
+def sanitize_console_name(console_name):
+    sanitized_name = re.sub('[^0-9a-zA-Z]+', '', console_name)
+    return sanitized_name.lower()
+
+def update_presence(RPC, data, game_data, start_time, username):
+    release_date = datetime.strptime(game_data['Released'], '%B %d, %Y')
+    year_of_release = release_date.year
+    details = f"{game_data['GameTitle']} ({year_of_release})"
+    RPC.update(
+        state=data["RichPresenceMsg"],
+        details=details,
+        start=start_time,
+        large_image="ra_logo",
+        large_text=f"Released {game_data['Released']}, Developed by {game_data['Developer']}, Published by {game_data['Publisher']}",
+        small_image=sanitize_console_name(game_data['ConsoleName']),
+        small_text=game_data['ConsoleName'],
+        buttons=[{"label": "View RA Profile", "url": f"https://retroachievements.org/user/{username}"}]
+    )
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    
+    username = config.get('DISCORD', 'username')
+    api_key = config.get('DISCORD', 'api_key')
+    client_id = config.get('DISCORD', 'client_id') if config.has_option('DISCORD', 'client_id') else "1249693940299333642"
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', action='store_true', help='Print debug information')
+    parser.add_argument('--fetch', type=int, default=30, help='Time to sleep before fetches in seconds')
+    args = parser.parse_args()
+
+    profile_url = f"https://retroachievements.org/API/API_GetUserProfile.php?u={username}&y={api_key}&z={username}"
+
+    start_time = int(time.time())
+
+    RPC = Presence(client_id)
+    print(Fore.CYAN + "Connecting to Discord App...")
+    RPC.connect()
+    print(Fore.MAGENTA + "Connected!")
+
+    while True:
+        print(Fore.CYAN + f"Fetching {username}'s RetroAchievements activity...")
+        data = get_data(profile_url)
+        if data is None:
+            break
+
+        print(Fore.MAGENTA + f"Result: {data['RichPresenceMsg']}")
+
+        game_params = f"?z={username}&y={api_key}&i={data['LastGameID']}"
+        game_url = f"https://retroachievements.org/API/API_GetGame.php{game_params}"
+        print(Fore.CYAN + "Fetching game data...")
+        game_data = get_data(game_url)
+        if game_data is None:
+            break
+
+        print(Fore.MAGENTA + f"Result: {game_data['GameTitle']}")
+
+        if args.debug:
+            print(Fore.YELLOW + "Debug game data:")
+            pprint(game_data)
+
+        update_presence(RPC, data, game_data, start_time, username)
+
+        print(Fore.CYAN + f"Sleeping for {args.fetch} seconds...")
+        time.sleep(args.fetch)
+
+if __name__ == "__main__":
+    main()
